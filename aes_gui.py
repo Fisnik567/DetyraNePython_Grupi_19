7e5780d98b5717253a129bea0db046f0d4c37b2e
GentritMahmuti <65553477+gentrit1234@users.noreply.github.com>
2021-02-13 16:07:01 +0100

    Update aes_gui.py

--- aes_gui.py
+++ aes_gui.py
@@ -134,8 +134,8 @@ tab1 = ttk.Frame(tab_control)
 tab2 = ttk.Frame(tab_control)
 tab3 = ttk.Frame(tab_control)
 
-tab_control.add(tab1, text=f'{"AES 192-bit encryption":^20s}')
-tab_control.add(tab2, text=f'{"AES 128-bit encryption":^20s}')
+tab_control.add(tab1, text=f'{"AES 128-bit encryption":^20s}')
+tab_control.add(tab2, text=f'{"AES 192-bit encryption":^20s}')
 tab_control.add(tab3, text=f'{"AES 256-bit encryption":^20s}')
 
 tab_control.pack(expand=1, fill="both")
